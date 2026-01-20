9bb37430f4d94a43fb4594c3cc0642e19e2be04e
Arijit Biswas <arijitbiswas476@gmail.com>
2026-01-20 22:30:46 +0530

    Add project files.

---
+++ MiniWebApp/wwwroot/js/app.js
@@ -0,0 +1,5 @@
+﻿document.getElementById("btn").addEventListener("click", async () => {
+    const res = await fetch("/api/hello");
+    const data = await res.json();
+    document.getElementById("output").textContent = JSON.stringify(data, null, 2);
+});
