3b3aaf932634fb9fbc5a09416b55711791a030d9
Arijit Biswas <arijitbiswas476@gmail.com>
2026-01-25 21:04:58 +0530

    initial projects

--- MiniWebApp/wwwroot/js/app.js
+++
@@ -1,5 +0,0 @@
-﻿document.getElementById("btn").addEventListener("click", async () => {
-    const res = await fetch("/api/hello");
-    const data = await res.json();
-    document.getElementById("output").textContent = JSON.stringify(data, null, 2);
-});
